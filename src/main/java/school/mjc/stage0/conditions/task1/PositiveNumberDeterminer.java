f14e53a0cacf252dbf594b0ee9e5ca4ffd0406f6
Maryna Kidun <marykidun@gmail.com>
2022-10-24 22:44:34 +0300

    Improved implementation

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.conditions.task1;
 public class PositiveNumberDeterminer {
     public static void isPositive(int numberToBeDetermined) {
         if (numberToBeDetermined > 0) {
-            System.out.println("The number is positive");
+            System.out.println("true");
         } else {
-            System.out.println("The number is negative");
+            System.out.println("false");
         }
     }
 
